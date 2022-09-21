24e3fc4c7477c1a8909358880542d858c17a5f0d
Chaejung Kim <whenucan35@gmail.com>
2022-09-21 22:30:51 +0900

    [ADD] getAccount 추가 (#4)
    * [ADD] storage util 생성
    
    - storage를 객체로 생성하여 필요한 메서드를 담아 따로 빼놓았습니다
    
    * [ADD] axios api
    
    - 필요한 url 정보와 axios 관련 요청들을 만들었습니다
    
    * [ADD] accountList fetch 추가
    
    - useEffect로 먼저 get 요청을 실행합니다

--- preface/src/api/index.js
+++ preface/src/api/index.js
@@ -1,20 +1,62 @@
 import axios from 'axios'
+import { storage } from 'util/storage'
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ld2ZhY2VAZGNvLmNvbSIsImlhdCI6MTY2Mzc0MjU4NywiZXhwIjoxNjYzNzQ2MTg3LCJzdWIiOiIxMDEifQ.AJcA64RoRad9qahdGYH9lod2rWUuOb7K12pRVb6cJXU'
+const BACKEND_PORT_NUMBER = process.env.REACT_APP_SERVER_PORT || '4000'
+const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL + ':' + BACKEND_PORT_NUMBER + '/' ||
+  'http://localhost:' + BACKEND_PORT_NUMBER + '/'
+const ACCESS_TOKEN =
+  storage.get().access_token ||
+  `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ld2ZhY2VAZGNvLmNvbSIsImlhdCI6MTY2Mzc2MzQ2MywiZXhwIjoxNjYzNzY3MDYzLCJzdWIiOiIxMDEifQ.pdV0cCbXgSLeyh1dfxMI6hBcgVUDWSauPNao-QxQgGU`
 
-const baseAxios = axios.create({
-  baseURL: 'http://localhost:4000',
+const Axios = axios.create({
+  baseURL: SERVER_URL,
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+    'Content-type': 'application/json',
   },
 })
 
+export const login = async loginData => {
+  const bodyData = JSON.stringify(loginData)
+  return await Axios.post('login', bodyData)
+}
+
+export const getAccounts = async () => {
+  return await Axios.get('/accounts')
+}
+
+export const editAccount = async editedData => {
+  const bodyData = JSON.stringify(editedData)
+  return await Axios.put('accounts', editedData)
+}
+
+export const deleteAccount = async uuid => {
+  return await Axios.delete(`accounts?uuid=${uuid}`)
+}
+
 export const getUserList = async () => {
-  const res = await baseAxios({
-    method: 'GET',
-    url: '/users',
-  })
-  console.log(res)
-  return res
+  return await Axios.get('users')
+}
+
+export const addUser = async userData => {
+  const bodyData = JSON.stringify(userData)
+  return await Axios.post('users', bodyData)
+}
+
+export const editUser = async editedData => {
+  const bodyData = JSON.stringify(editedData)
+  return await Axios.put('users', bodyData)
+}
+
+export const deleteUser = async uuid => {
+  return await Axios.delete(`users?uuid=${uuid}`)
+}
+
+export const getUserSetting = async () => {
+  return await Axios.get('userSetting')
+}
+
+export const searchUsers = async word => {
+  return await Axios.get(`users?q=${word}`)
 }
