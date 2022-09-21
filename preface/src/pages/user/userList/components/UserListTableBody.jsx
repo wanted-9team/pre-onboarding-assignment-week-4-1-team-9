da5a537c11257616711ffac3a790cd22934d004e
sacultang <sacultang@gmail.com>
2022-09-22 06:26:39 +0900

    [RFT] 안쓰는 코드 삭제 order,orderby 삭제

--- preface/src/pages/user/userList/components/UserListTableBody.jsx
+++ preface/src/pages/user/userList/components/UserListTableBody.jsx
@@ -2,23 +2,13 @@ import React from 'react'
 import TableBody from '@mui/material/TableBody'
 import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
-import Radio from '@mui/material/Radio'
+import Checkbox from '@mui/material/Checkbox'
 import Button from '@mui/material/Button'
 import { toLocaleDateFunc, transLoginTimeFunc } from 'utils/transDate'
 import { maskingPhoneNumber } from 'utils/maskingNumber'
-import Comparator from 'utils/Comparator'
 import { useNavigate } from 'react-router-dom'
-const { stableSort, getComparator } = Comparator
 
-const UserListTableBody = ({
-  userData,
-  order,
-  orderBy,
-  page,
-  rowsPerPage,
-  selected,
-  setSelected,
-}) => {
+const UserListTableBody = ({ userData, selected, setSelected }) => {
   const navigate = useNavigate()
 
   const goUserDetails = user => {
@@ -33,50 +23,46 @@ const UserListTableBody = ({
 
   return (
     <TableBody>
-      {stableSort(userData, getComparator(order, orderBy))
-        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-        .map((user, index) => {
-          const labelId = `enhanced-table-checkbox-${index}`
-          const isItemSelected = isSelected(selected.uuid, user.uuid)
-          return (
-            user.uuid && (
-              <TableRow
-                hover
-                tabIndex={-1}
-                key={user.uuid}
-                aria-checked={isItemSelected}
-                selected={isItemSelected}
-              >
-                <TableCell padding="checkbox">
-                  <Radio
-                    checked={isItemSelected}
-                    onChange={() => handleSelectUser(user)}
-                    value="a"
-                    name="radio-buttons"
-                    inputProps={{ 'aria-label': 'A' }}
-                  />
-                </TableCell>
-                <TableCell id={labelId} scope="row" padding="normal">
-                  {user.name}
-                </TableCell>
-                <TableCell align="left">{user.accountList.length}</TableCell>
-                <TableCell align="left">{user.email}</TableCell>
-                <TableCell align="left">{toLocaleDateFunc(user.birth_date)}</TableCell>
-                <TableCell align="left">{maskingPhoneNumber(user.phone_number)}</TableCell>
-                <TableCell align="left">{user.gender_origin}</TableCell>
-                <TableCell align="left">{transLoginTimeFunc(user.last_login)}</TableCell>
-                <TableCell align="left">{user.allow_marketing_push ? 'Yes' : 'No'}</TableCell>
-                <TableCell align="left">{user.is_active ? 'Yes' : 'No'}</TableCell>
-                <TableCell align="left">{toLocaleDateFunc(user.created_at)}</TableCell>
-                <TableCell align="left">
-                  <Button variant="outlined" size="small" onClick={() => goUserDetails(user)}>
-                    상세 보기
-                  </Button>
-                </TableCell>
-              </TableRow>
-            )
+      {userData.map((user, index) => {
+        const labelId = `enhanced-table-checkbox-${index}`
+        const isItemSelected = isSelected(selected.uuid, user.uuid)
+        return (
+          user.uuid && (
+            <TableRow
+              hover
+              tabIndex={-1}
+              key={user.uuid}
+              aria-checked={isItemSelected}
+              selected={isItemSelected}
+            >
+              <TableCell padding="checkbox" role={'checkbox'}>
+                <Checkbox
+                  checked={isItemSelected}
+                  onChange={() => handleSelectUser(user)}
+                  name="checkbox-buttons"
+                />
+              </TableCell>
+              <TableCell id={labelId} scope="row" align="center">
+                {user.name}
+              </TableCell>
+              <TableCell align="center">{user.accountList.length}</TableCell>
+              <TableCell align="center">{user.email}</TableCell>
+              <TableCell align="center">{maskingPhoneNumber(user.phone_number)}</TableCell>
+              <TableCell align="center">{toLocaleDateFunc(user.birth_date)}</TableCell>
+              <TableCell align="center">{user.gender_origin}</TableCell>
+              <TableCell align="center">{user.is_active ? 'Yes' : 'No'}</TableCell>
+              <TableCell align="center">{user.allow_marketing_push ? 'Yes' : 'No'}</TableCell>
+              <TableCell align="center">{toLocaleDateFunc(user.created_at)}</TableCell>
+              <TableCell align="center">{transLoginTimeFunc(user.last_login)}</TableCell>
+              <TableCell align="center">
+                <Button variant="outlined" size="small" onClick={() => goUserDetails(user)}>
+                  상세 보기
+                </Button>
+              </TableCell>
+            </TableRow>
           )
-        })}
+        )
+      })}
     </TableBody>
   )
 }
