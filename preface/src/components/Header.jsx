0d75cf226fe84ac4e1f42f2e85bc1c39a663e88d
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-09-21 15:13:16 +0900

    [ADD]라우팅 수정
    메인페이지와 Login 페이지를 나누어 라우팅

--- preface/src/components/Header.jsx
+++ preface/src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { styled } from '@mui/material'
 import React from 'react'
 
 function Header() {
-  return <div>Header</div>
+  return <HeaderContainer>Header</HeaderContainer>
 }
 
 export default Header
+
+const HeaderContainer = styled('div')({
+  width: '100%',
+  height: 80,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
