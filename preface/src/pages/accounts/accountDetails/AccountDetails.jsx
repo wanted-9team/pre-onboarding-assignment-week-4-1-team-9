43f75819c2ea994b4c415ade3273aa0cf963b134
Jaehyeon <jaehyeonp74@gmail.com>
2022-09-22 18:23:15 +0900

    [ADD] 상세페이지 삭제 기능 구현

--- preface/src/pages/accounts/accountDetails/AccountDetails.jsx
+++ preface/src/pages/accounts/accountDetails/AccountDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getAccountDetail, getUserDetail } from 'api'
+import { useParams, useNavigate } from 'react-router-dom'
+import { getAccountDetail, getUserDetail, editAccount, deleteAccount } from 'api'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -13,11 +13,32 @@ import brokers from 'data/brokers.json'
 import accountStatus from 'data/accountStatus.json'
 import getKeyByValue from 'utils/getKeyByValue'
 import { toLocaleDateFunc } from 'utils/transDate'
+import Box from '@mui/material/Box'
+import EarningsRate from 'utils/EarningsRate'
 
 function AccountDetails() {
+  const navigate = useNavigate()
   const accountId = useParams().id
   const [accountDetail, setAccountDetail] = useState({})
   const [editMode, setEditMode] = useState(false)
+
+  const onClickEditMode = () => {
+    setEditMode(prev => !prev)
+  }
+
+  const onChangeHandler = event => {
+    const { value, name } = event.target
+    setAccountDetail({ ...accountDetail, [name]: value })
+  }
+
+  const onClickEditComplete = async detail => {
+    const id = detail.id
+  }
+
+  const onClickDeleteDetail = async id => {
+    await deleteAccount(id)
+    await navigate('/main')
+  }
 
   useEffect(() => {
     const fetchAccountDetail = async id => {
@@ -30,6 +51,7 @@ function AccountDetails() {
           broker_id: brokers[accountListRes.data.broker_id],
           status: getKeyByValue(accountStatus, accountListRes.data.status),
           created_at: toLocaleDateFunc(accountListRes.data.created_at),
+          earnings_rate: EarningsRate(accountListRes.data.assets, accountListRes.data.payments),
         }
         setAccountDetail(newAccountDetail)
       } catch (e) {
@@ -41,55 +63,77 @@ function AccountDetails() {
 
   return (
     <>
-      {!editMode ? (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>사용자명</TableCell>
-                <TableCell align="right">브로커 명</TableCell>
-                <TableCell align="right">계좌 상태</TableCell>
-                <TableCell align="right">계좌번호</TableCell>
-                <TableCell align="right">계좌명</TableCell>
-                <TableCell align="right">평가 금액</TableCell>
-                <TableCell align="right">입금 금액</TableCell>
-                <TableCell align="right">계좌 활성화 여부</TableCell>
-                <TableCell align="right">계좌 개설일</TableCell>
-                <TableCell align="right">Edit</TableCell>
-                <TableCell align="right">Delete</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">
-                  {accountDetail?.userName}
-                </TableCell>
-                <TableCell align="right">{accountDetail.broker_id}</TableCell>
-                <TableCell align="right">{accountDetail.status}</TableCell>
-                <TableCell align="right">{accountDetail.number}</TableCell>
-                <TableCell align="right">{accountDetail.name}</TableCell>
-                <TableCell align="right">{accountDetail.assets}</TableCell>
-                <TableCell align="right">{accountDetail.payments}</TableCell>
-                <TableCell align="right">{accountDetail.is_active ? '사용중' : '사용X'}</TableCell>
-                <TableCell align="right">{accountDetail.created_at}</TableCell>
-
-                {/* <TableCell align="right">
-                  <Button fullWidth variant="contained">
-                    Edit
-                  </Button>
-                  ;
-                </TableCell>
-                <TableCell align="right">
-                  <Button fullWidth variant="contained">
-                    Delete
-                  </Button>
-                  ;
-                </TableCell> */}
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      ) : null}
+      <TableContainer component={Paper} sx={{ width: '100%', height: '100%', margin: '60px 0' }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">사용자명</TableCell>
+              <TableCell align="center">브로커 명</TableCell>
+              <TableCell align="center">계좌 상태</TableCell>
+              <TableCell align="center">계좌번호</TableCell>
+              <TableCell align="center">계좌명</TableCell>
+              <TableCell align="center">평가 금액</TableCell>
+              <TableCell align="center">입금 금액</TableCell>
+              <TableCell align="center">계좌 활성화 여부</TableCell>
+              <TableCell align="center">수익률</TableCell>
+              <TableCell align="center">계좌 개설일</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              {!editMode ? (
+                <>
+                  <TableCell component="th" scope="row">
+                    {accountDetail?.userName}
+                  </TableCell>
+                  <TableCell align="center">{accountDetail.broker_id}</TableCell>
+                  <TableCell align="center">{accountDetail.status}</TableCell>
+                  <TableCell align="center">{accountDetail.number}</TableCell>
+                  <TableCell align="center">{accountDetail.name}</TableCell>
+                  <TableCell align="center">{accountDetail.assets}</TableCell>
+                  <TableCell align="center">{accountDetail.payments}</TableCell>
+                  <TableCell align="center">
+                    {accountDetail.is_active ? '사용중' : '사용X'}
+                  </TableCell>
+                  <TableCell align="center">{accountDetail.earnings_rate}</TableCell>
+                  <TableCell align="center">{accountDetail.created_at}</TableCell>
+                </>
+              ) : null}
+            </TableRow>
+          </TableBody>
+        </Table>
+        {!editMode ? (
+          <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+            <Box>
+              <Button variant="contained" onClick={onClickEditMode}>
+                편집
+              </Button>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => {
+                  onClickDeleteDetail(accountId)
+                }}
+              >
+                삭제
+              </Button>
+            </Box>
+          </Box>
+        ) : (
+          <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+            <Box>
+              <Button variant="contained">저장</Button>
+            </Box>
+            <Box>
+              <Button variant="contained" color="error" onClick={onClickEditMode}>
+                취소
+              </Button>
+            </Box>
+          </Box>
+        )}
+      </TableContainer>
     </>
   )
 }
