e6f781559f7a8210e3ffd100b9e051ab50a5df3a
Jaehyeon <jaehyeonp74@gmail.com>
2022-09-22 18:03:45 +0900

    [ADD] sever.js에 cors 처리

--- data_generate/server.ts
+++ data_generate/server.ts
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server')
 
 const auth = require('json-server-auth')
+const cors = require('cors')
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -16,6 +17,7 @@ app.use((req: any, res: any, next: any) => {
   res.header('Access-Control-Allow-Headers', '*')
   next()
 })
+app.use(cors())
 app.use(rules)
 app.use(auth)
 app.use(router)
