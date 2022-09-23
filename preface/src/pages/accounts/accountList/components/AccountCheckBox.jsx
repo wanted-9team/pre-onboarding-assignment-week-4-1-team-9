26b805e1d10ebd59393d5185b6b727e5d9ed822d
chaejung Kim <whenucan35@gmail.com>
2022-09-23 16:10:07 +0900

    [ADD] checkBox에 증권사 연결
    - select에 증권사 목록을 연결

--- preface/src/pages/accounts/accountList/components/AccountCheckBox.jsx
+++ preface/src/pages/accounts/accountList/components/AccountCheckBox.jsx
@@ -4,18 +4,29 @@ import TextField from '@mui/material/TextField'
 import Autocomplete from '@mui/material/Autocomplete'
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
 import CheckBoxIcon from '@mui/icons-material/CheckBox'
+import brokers from 'data/brokers.json'
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
 const checkedIcon = <CheckBoxIcon fontSize="small" />
 
 export default function AccountCheckbox() {
+  const makeBrokerData = () => {
+    let newArray = []
+    for (const broker_number in brokers) {
+      newArray.push({ name: brokers[broker_number], number: broker_number })
+    }
+    return newArray
+  }
+  const brokerData = makeBrokerData()
+  console.log(brokerData)
+
   return (
     <Autocomplete
       multiple
       id="checkboxes-tags-demo"
-      options={top100Films}
+      options={brokerData}
       disableCloseOnSelect
-      getOptionLabel={option => option.title}
+      getOptionLabel={option => option.name}
       renderOption={(props, option, { selected }) => (
         <li {...props}>
           <Checkbox
@@ -24,7 +35,7 @@ export default function AccountCheckbox() {
             style={{ marginRight: 8 }}
             checked={selected}
           />
-          {option.title}
+          {option.name}
         </li>
       )}
       style={{ width: 500 }}
@@ -32,54 +43,3 @@ export default function AccountCheckbox() {
     />
   )
 }
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-]
