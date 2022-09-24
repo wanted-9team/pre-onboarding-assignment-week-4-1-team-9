153170c6969b5ffbbc602a92d747c8d7e81b3143
OH YOUNGJAE <85508157+sacultang@users.noreply.github.com>
2022-09-25 03:32:23 +0900

    [RFT] maskingName util 재작성 (#43)
    공백처리 및 두글자 세글자 케이스 추가

--- preface/src/utils/maskingName.js
+++ preface/src/utils/maskingName.js
@@ -1,25 +1,14 @@
 export const maskingName = name => {
   const star = '*'
-  const includeKor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/
-  let nameArray = name.split(' ')
+  let nameArray = name.split(' ').join('').split('')
   if (nameArray.length === 2) {
     nameArray[0] = star.repeat(nameArray[0].length)
-  } else {
-    for (let i = 0; i < nameArray.length - 1; i++) {
-      if (
-        nameArray[i] === 'Mr.' ||
-        nameArray[i] === 'Ms.' ||
-        nameArray[i] === 'Miss' ||
-        nameArray[i] === 'Mrs.' ||
-        nameArray[i] === 'Dr.'
-      ) {
-        continue
-      } else if (includeKor.test(nameArray[i])) {
-        break
-      } else {
-        nameArray[i] = star.repeat(nameArray[i].length)
-      }
+  } else if (nameArray.length === 3) {
+    nameArray[1] = star.repeat(nameArray[0].length)
+  } else if (nameArray.length >= 4) {
+    for (let i = 1; i < nameArray.length - 1; i++) {
+      nameArray[i] = star.repeat(nameArray[i].length)
     }
   }
-  return nameArray.join(' ')
+  return nameArray.join('')
 }
