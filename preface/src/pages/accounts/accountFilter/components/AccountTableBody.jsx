f3f339a1fd8bea95710bed1e44dcfc2804b972cd
chaejung Kim <whenucan35@gmail.com>
2022-09-25 06:17:04 +0900

    [STY] 긍부정 폰트 색 스타일링
    - 활성화, 수익률에는 긍정 색으로
    - 비활성화, 관리자확인필요 등에는 부정 색으로 스타일링

--- preface/src/pages/accounts/accountFilter/components/AccountTableBody.jsx
+++ preface/src/pages/accounts/accountFilter/components/AccountTableBody.jsx
@@ -10,6 +10,7 @@ import getFormattedPrice from 'utils/getFormattedPrice'
 import { toLocaleDateFunc } from 'utils/transDate'
 import { getFormattedAccountNumber } from 'utils/getFormattedAccountNumber'
 import { maskingAccount } from 'utils/maskingAccountNumber'
+import theme from 'theme'
 
 function AccountTableBody({ rows, page, rowsPerPage, onClick }) {
   const navigate = useNavigate()
@@ -29,21 +30,38 @@ function AccountTableBody({ rows, page, rowsPerPage, onClick }) {
 
           return (
             <TableRow hover role="checkbox" tabIndex={-1} key={index} align="center">
-              <TableCell component="th" scope="row" padding="none">
+              <TableCell component="th" scope="row" margin="2">
                 {row.user_name}
               </TableCell>
               <TableCell>{toBrokerName(row.broker_id)}</TableCell>
               <TableCell onClick={() => goAccountDetail(row.id)}>
                 {maskingAccount(getFormattedAccountNumber(row.broker_id, row.number))}
               </TableCell>
-              <TableCell>{toStatusString(row.status)}</TableCell>
+              <TableCell
+                sx={{
+                  color:
+                    row.status === 9999 ? theme.palette.error.main : theme.palette.primary.main,
+                }}
+              >
+                {toStatusString(row.status)}
+              </TableCell>
               <TableCell>{row.name}</TableCell>
               <TableCell>{getFormattedPrice(row.assets)}</TableCell>
               <TableCell>{getFormattedPrice(row.payments)}</TableCell>
-              <TableCell sx={{ color: `${earningsRate >= 0 ? 'blue' : 'red'}` }}>
+              <TableCell
+                sx={{
+                  color: earningsRate >= 0 ? theme.palette.primary.main : theme.palette.error.main,
+                }}
+              >
                 {earningsRate}%
               </TableCell>
-              <TableCell>{row.is_active ? 'Yes' : 'No'}</TableCell>
+              <TableCell
+                sx={{
+                  color: row.is_active ? theme.palette.primary.main : theme.palette.error.main,
+                }}
+              >
+                {row.is_active ? 'Yes' : 'No'}
+              </TableCell>
               <TableCell>{toLocaleDateFunc(row.created_at)}</TableCell>
             </TableRow>
           )
