b2cf84094f0b61af3112ba488f5d04b81357a2aa
chaejung Kim <whenucan35@gmail.com>
2022-09-25 06:18:02 +0900

    [FIX] 계좌번호 마스킹 함수 수정
    - '-'가 마지막 두번째에 오는 경우 끝 두자리 수가 마스킹 되지 않는 경우에 대한 처리를 했습니다

--- preface/src/utils/maskingAccountNumber.js
+++ preface/src/utils/maskingAccountNumber.js
@@ -1,8 +1,11 @@
 export const maskingAccount = account => {
   const star = '*'
   let accountArray = account.split('')
-  for (let i = 1; i < accountArray.length - 1; i++) {
+  for (let i = 2; i < accountArray.length - 2; i++) {
     if (accountArray[i] !== '-') {
+      if (i === accountArray.length - 3 && accountArray[i + 1] === '-') {
+        break
+      }
       accountArray[i] = star.repeat(accountArray[i].length)
     }
   }
